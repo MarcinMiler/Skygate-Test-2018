53ee6b8dfd655ab82a1a06102ea8817e4ca0830e
Marcin Miler <marcin4951@gmail.com>
2018-06-04 08:22:07 +0200

    Fixing scroll problem when page has changed

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './Views/Home'
 import Navbar from './Components/Navbar'
 import AddEvent from './Views/AddEvent'
 import ExploreEvents from './Views/ExploreEvents'
 
+const ScrollToTop = () => {
+    window.scrollTo(0, 0)
+    return null
+}
+
 const Routes = () => (
-    <BrowserRouter>
+    <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
         <div>
             <Navbar />
-
-            <Route exact path="/" component={Home} />
-            <Route exact path="/addEvent" component={AddEvent} />
-            <Route exact path="/expolreEvents" component={ExploreEvents} />
+            <Route component={ScrollToTop} />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/addEvent" component={AddEvent} />
+                <Route exact path="/expolreEvents" component={ExploreEvents} />
+            </Switch>
         </div>
     </BrowserRouter>
 )
