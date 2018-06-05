8ff0aa1a31f97e165a3694cbee143a75b5f38030
Marcin Miler <marcin4951@gmail.com>
2018-06-05 09:27:31 +0200

    Setup nodejs server

---
+++ server/src/entity/User.ts
@@ -0,0 +1,8 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn() id: number
+
+    @Column() firstName: string
+}
---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata'
+import { GraphQLServer } from 'graphql-yoga'
+import { createConnection } from 'typeorm'
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+    Query: {
+        hello: async (_: any, { name }: any) => `Hello ${name || 'World'}`
+    }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+
+createConnection().then(() => {
+    server.start(() => console.log('Server is running on localhost:4000'))
+})
